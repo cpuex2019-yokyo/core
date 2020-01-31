e6b8b45e1f39001da17b611e7a4f9f531ee3606a
TakutoMitsunobu <t_mitsunobu@is.s.u-tokyo.ac.jp>
2020-01-31 20:46:12 +0900

    rename msip&mtip, 割り込み信号を立ちっぱなしに

--- src/clint.v
+++ src/clint.v
@@ -26,19 +26,20 @@ module clint(
 	input wire        axi_wvalid,
 
 	output reg [63:0] mtime,
-	output reg msip,
-	output reg mtip,
+	output reg software_intr,
+	output wire time_intr,
 
 	input wire clk,
 	input wire rstn
 );
 
-	localparam msip_addr = 32'h0;
+	localparam software_intr_addr = 32'h0;
 	localparam mtimecmp_addr = 32'h4000;
 	localparam mtime_addr = 32'hbff8;		// Read Only
 
 	reg [63:0] mtimecmp;
-	reg mtip_enable;
+
+	assign time_intr = mtimecmp <= mtime;
 
 	always @(posedge clk) begin
 		if(~rstn) begin
@@ -50,23 +51,15 @@ module clint(
 			axi_bvalid <= 1'b0;
 			axi_awready <= 1'b1;
 			axi_wready <= 1'b1;
-			mtip_enable <= 1'b1;
-			mtip <= 1'b0;
-			msip <= 1'b0;
+			software_intr <= 1'b0;
 			mtime <= 64'h0;
 		end else begin
 			mtime <= mtime + 64'h1;
-			mtip <= 1'b0;
-			if(mtime + 64'h1 >= mtimecmp) begin
-				mtip <= mtip_enable;
-				mtip_enable <= 1'b0;
-			end
-
 			if(axi_arvalid) begin
 				axi_rvalid <= 1'b1;
 				axi_rresp <= 2'b00;
-				if(axi_araddr == msip_addr) begin
-					axi_rdata <= {31'h0, msip};
+				if(axi_araddr == software_intr_addr) begin
+					axi_rdata <= {31'h0, software_intr};
 				end else if(axi_araddr == mtimecmp_addr) begin
 					axi_rdata <= mtimecmp[31:0];
 				end else if(axi_araddr == mtimecmp_addr + 32'h4) begin
@@ -85,20 +78,18 @@ module clint(
 			if(axi_awvalid && axi_wvalid) begin
 				axi_bvalid <= 1'b1;
 				axi_bresp <= 2'b00;
-				if(axi_awaddr == msip_addr) begin
-					if(axi_wstrb[0]) msip <= axi_wdata[0];
+				if(axi_awaddr == software_intr_addr) begin
+					if(axi_wstrb[0]) software_intr <= axi_wdata[0];
 				end else if(axi_awaddr == mtimecmp_addr) begin
 					if(axi_wstrb[0]) mtimecmp[7:0] <= axi_wdata[7:0];
 					if(axi_wstrb[1]) mtimecmp[15:8] <= axi_wdata[15:8];
 					if(axi_wstrb[2]) mtimecmp[23:16] <= axi_wdata[23:16];
 					if(axi_wstrb[3]) mtimecmp[31:24] <= axi_wdata[31:24];
-					mtip_enable <= 1'b1;
 				end else if(axi_awaddr == mtimecmp_addr + 32'h4) begin
 					if(axi_wstrb[0]) mtimecmp[39:32] <= axi_wdata[7:0];
 					if(axi_wstrb[1]) mtimecmp[47:40] <= axi_wdata[15:8];
 					if(axi_wstrb[2]) mtimecmp[55:48] <= axi_wdata[23:16];
 					if(axi_wstrb[3]) mtimecmp[63:56] <= axi_wdata[31:24];
-					mtip_enable <= 1'b1;
 				end else begin
 					axi_bresp <= 2'b10;
 				end
